1baeab4bba1e032008471dcbb723db29f8970fcb
Kolby Evans <77802339+FrootLoopJunkie@users.noreply.github.com>
2021-03-16 13:58:08 -0500

    Socket io basic setup complete

---
+++ server.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const httpServer = require('http').createServer(app);
+const io = require('socket.io')(httpServer);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('Client Connected');
+})
+
+httpServer.listen(process.env.PORT, () => {
+    console.log(`Now Listening On Port: ${process.env.PORT}`);
+})

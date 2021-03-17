89467df222278e2c18fa77125a80e8d419f04454
Kolby Evans <77802339+FrootLoopJunkie@users.noreply.github.com>
2021-03-17 18:12:43 -0500

    I tink I can I tink I can

--- public/app.js
+++ public/app.js
@@ -72,13 +72,7 @@ function newPost(arg){
     const regx = /#(\w+)\b/ig;
     const hashtags = arg.match(regx);
     console.log(hashtags)
-    if(currentRoomName !== '#home' && hashtags === null){
-        return;
-    }
     if(hashtags !== null){
-        if(currentRoomName !== '#home' && !hashtags.includes(currentRoomName)){
-            return;
-        }
         hashtags.forEach((elem) => {
             let hashtag = `<span class=hashtag>${elem}</span>`;
             arg = arg.replace(elem, hashtag)

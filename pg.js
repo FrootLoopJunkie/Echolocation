c02179b6e3444b11e1324a4688005134f1b28128
Kolby Evans <77802339+FrootLoopJunkie@users.noreply.github.com>
2021-03-16 22:41:15 -0500

    fixed db variables

--- pg.js
+++ pg.js
@@ -1,15 +1,10 @@
 const {Pool} = require('pg');
 
 const pool = new Pool({
-    user: 'Kolby',
-    password: '',
-    host: 'localhost',
-    port: 5432,
-    database: 'tempdb'
-    // connectionString: process.env.DATABASE_URL,
-    // ssl: {
-    //   rejectUnauthorized: false
-    // }
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
 })
 
 module.exports = pool;

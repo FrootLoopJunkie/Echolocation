a9ab4c7dc394a0941743759ee0bcd6725dc799df
Kolby Evans <77802339+FrootLoopJunkie@users.noreply.github.com>
2021-03-17 14:22:59 -0500

    Testing

--- public/app.js
+++ public/app.js
@@ -55,12 +55,12 @@ socket.on('newPost', (arg) => {
 socket.on('joinedRoom', (arg1, arg2) => {
     currentRoom.innerHTML = `Current Room: ${arg1}`
     $("#feedContainer").empty();
-    if(arg2 !== null){
-        const posts = arg2.rows; 
-        posts.forEach((elem) => {
-            newPost(elem.post_body);
-        })
-    }
+    // if(arg2 !== null){
+    //     const posts = arg2.rows; 
+    //     posts.forEach((elem) => {
+    //         newPost(elem.post_body);
+    //     })
+    // }
 })
 
 function newPost(arg){

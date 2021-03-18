520ae04fbb15d12c93788b7e28a85a7709f04a04
Kolby Evans <77802339+FrootLoopJunkie@users.noreply.github.com>
2021-03-17 23:08:50 -0500

    test

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ io.on('connection', async(socket) => {
     socket.on('newPost', async(arg, arg2) => {
         try{
             console.log(socket.rooms);
-            //socket.to('#home').emit('newPost', {'post_body': arg});
+            socket.to('#home').emit('newPost', {'post_body': arg});
             const client = await pool.connect();
             const privatePost = await pool.query(`INSERT INTO posts_private (post_contents, date_created, user_id) VALUES ('${arg}', null, '1'); SELECT currval(pg_get_serial_sequence('posts_private', 'post_id'))`); 
             const postID = privatePost[1].rows[0].currval;
@@ -55,7 +55,7 @@ io.on('connection', async(socket) => {
                     console.log(`Adding ${elem} To DB`)
                     const hashtagInsert = await pool.query(`INSERT INTO post_hashtags (hashtag, post_id) VALUES ('${elem}', '${postID}')`); 
                     if(elem !== socket.id){
-                        socket.to(elem).emit('newPost', {'post_body': arg});
+                        //socket.to(elem).emit('newPost', {'post_body': arg});
                     }
                     if(!hashtagArray.includes(elem)){
                         hashtagArray.push(elem);
